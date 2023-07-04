4b656af1d05032b727ee4e2363c61c056b6f422c
Audrius Ivko <audrius.ivko@gmail.com>
2023-07-04 17:11:04 +0300

    Allert

--- resources/views/alerts/alert-success.blade.php
+++ resources/views/alerts/alert-success.blade.php
@@ -8,21 +8,3 @@
         @endif
     </div>
 </section>
-
-
-{{-- @if (\Session::has('success'))
- <div class="alert alert-success">
-     <p>{{ \Session::get('success') }}</p>
-</div>
-@endif --}}
-
-{{-- @if (count($errors) > 0)
- <div class="alert alert-danger">
-     <strong>Opps!</strong> Something went wrong, please check below errors.<br><br>
-     <ul>
-         @foreach ($errors->all() as $error)
-         <li>{{ $error }}</li>
-@endforeach
-</ul>
-</div>
-@endif --}}
