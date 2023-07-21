583894de15db66324c61cad65b5dac9341d5a192
Audrius Ivko <audrius.ivko@gmail.com>
2023-07-21 13:43:10 +0300

    Board css

--- resources/views/front/all.blade.php
+++ resources/views/front/all.blade.php
@@ -18,19 +18,19 @@
                     @endcan
                 </div>
             </div>
-            <div class="card h-100 p-3">
+            <div class="card-body h-100">
                 <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-4 row-cols-xxl-4 g-4">
                     <div class="col">
-                        <div class="card h-100">
-                            <div class="card-header"> TO DO</div>
+                        <div class="card bg-dark-subtle h-100">
+                            <div class="card-header">TO DO</div>
                             @foreach ($data as $key => $open)
                                 @if ($open->status_id == 1)
-                                    <div class="card m-1">
+                                    <div class="card m-2 board-card">
                                         <a class=" text-decoration-none text-black"
                                             href="{{ route('tickets.show', $open->id) }}">
-                                            <div class="card-body">
+                                            <div class="card-body shadow">
                                                 <div class="d-flex">
-                                                    <div class="board-pill"
+                                                    <div class="board-pill shadow"
                                                         style="background-color:{{ $open->ticketsImportant->important_bc }};">
                                                         @if ($open->attach_json !== null)
                                                             <i class="bi bi-paperclip fs-4 board-att"
@@ -41,22 +41,19 @@
                                                     {{-- <img src="..." class="card-img-top" alt="..."> --}}
                                                     <h6 class="card-text d-flex"> {{ $open->ticketsUser->name }}</h6>
                                                 </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50">
-                                                    Open: {{ $open->created_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50 ">
-                                                    Edit : {{ $open->updated_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <h6>{{ $open->title }}</h6>
+                                                <h6 class="mt-3 fw-light">{{ $open->title }}</h6>
                                                 {{-- <p> {!! nl2br(e($open->title)) !!}</p> --}}
                                                 {{-- <h6 class="length_title">{{ $open->title }}</h6> --}}
                                                 {{-- <samp class="card-title">{{ $open->title }}</samp> --}}
                                                 {{-- <p class="card-text">{{ $open->user_id }}</p> --}}
-                                                <p class="length_message">{{ $open->request }}</p>
+                                                <p class="length_message fw-lighter">{{ $open->request }}</p>
                                                 @can('ticket-edit')
                                                     <a class="icon-edit text-decoration-none fs-2 text-black-50 float-end"
                                                         href="{{ route('tickets.edit', $open->id) }}"></a>
                                                 @endcan
+                                                <div class="py-1 fs-6 d-flex flex-row text-black-50 font-monospace">
+                                                    {{ $open->created_at->format('Y-m-d, H:m') }}
+                                                </div>
                                             </div>
                                         </a>
                                         @can('delete-btn')
@@ -83,16 +80,16 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card h-100">
+                        <div class="card bg-dark-subtle h-100">
                             <div class="card-header">IN PROGRESS</div>
                             @foreach ($data as $open)
                                 @if ($open->status_id == 2)
-                                    <div class="card m-1">
+                                    <div class="card m-2 board-card">
                                         <a class=" text-decoration-none text-black"
                                             href="{{ route('tickets.show', $open->id) }}">
-                                            <div class="card-body">
+                                            <div class="card-body shadow">
                                                 <div class="d-flex">
-                                                    <div class="board-pill"
+                                                    <div class="board-pill shadow"
                                                         style="background-color:{{ $open->ticketsImportant->important_bc }};">
                                                         @if ($open->attach_json !== null)
                                                             <i class="bi bi-paperclip fs-4 board-att"
@@ -103,22 +100,19 @@
                                                     {{-- <img src="..." class="card-img-top" alt="..."> --}}
                                                     <h6 class="card-text d-flex"> {{ $open->ticketsUser->name }}</h6>
                                                 </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50">
-                                                    Open: {{ $open->created_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50 ">
-                                                    Edit : {{ $open->updated_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <h6>{{ $open->title }}</h6>
+                                                <h6 class="mt-3 fw-light">{{ $open->title }}</h6>
                                                 {{-- <p> {!! nl2br(e($open->title)) !!}</p> --}}
                                                 {{-- <h6 class="length_title">{{ $open->title }}</h6> --}}
                                                 {{-- <samp class="card-title">{{ $open->title }}</samp> --}}
                                                 {{-- <p class="card-text">{{ $open->user_id }}</p> --}}
-                                                <p class="length_message">{{ $open->request }}</p>
+                                                <p class="length_message fw-lighter">{{ $open->request }}</p>
                                                 @can('ticket-edit')
                                                     <a class="icon-edit text-decoration-none fs-2 text-black-50 float-end"
                                                         href="{{ route('tickets.edit', $open->id) }}"></a>
                                                 @endcan
+                                                <div class="py-1 fs-6 d-flex flex-row text-black-50 font-monospace">
+                                                    {{ $open->updated_at->format('Y-m-d, H:m') }}
+                                                </div>
                                             </div>
                                         </a>
                                         @can('delete-btn')
@@ -145,16 +139,16 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card h-100">
+                        <div class="card bg-dark-subtle h-100">
                             <div class="card-header">WAIT ANSWER</div>
                             @foreach ($data as $key => $open)
                                 @if ($open->status_id == 3)
-                                    <div class="card m-1">
+                                    <div class="card m-2 board-card">
                                         <a class=" text-decoration-none text-black"
                                             href="{{ route('tickets.show', $open->id) }}">
-                                            <div class="card-body">
+                                            <div class="card-body shadow">
                                                 <div class="d-flex">
-                                                    <div class="board-pill"
+                                                    <div class="board-pill shadow"
                                                         style="background-color:{{ $open->ticketsImportant->important_bc }};">
                                                         @if ($open->attach_json !== null)
                                                             <i class="bi bi-paperclip fs-4 board-att"
@@ -165,22 +159,19 @@
                                                     {{-- <img src="..." class="card-img-top" alt="..."> --}}
                                                     <h6 class="card-text d-flex"> {{ $open->ticketsUser->name }}</h6>
                                                 </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50">
-                                                    Open: {{ $open->created_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50 ">
-                                                    Edit : {{ $open->updated_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <h6>{{ $open->title }}</h6>
+                                                <h6 class="mt-3 fw-light">{{ $open->title }}</h6>
                                                 {{-- <p> {!! nl2br(e($open->title)) !!}</p> --}}
                                                 {{-- <h6 class="length_title">{{ $open->title }}</h6> --}}
                                                 {{-- <samp class="card-title">{{ $open->title }}</samp> --}}
                                                 {{-- <p class="card-text">{{ $open->user_id }}</p> --}}
-                                                <p class="length_message">{{ $open->request }}</p>
+                                                <p class="length_message fw-lighter">{{ $open->request }}</p>
                                                 @can('ticket-edit')
                                                     <a class="icon-edit text-decoration-none fs-2 text-black-50 float-end"
                                                         href="{{ route('tickets.edit', $open->id) }}"></a>
                                                 @endcan
+                                                <div class="py-1 fs-6 d-flex flex-row text-black-50 font-monospace">
+                                                    {{ $open->updated_at->format('Y-m-d, H:m') }}
+                                                </div>
                                             </div>
                                         </a>
                                         @can('delete-btn')
@@ -207,16 +198,16 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card h-100">
+                        <div class="card bg-dark-subtle h-100">
                             <div class="card-header">DONE</div>
                             @foreach ($data as $key => $open)
                                 @if ($open->status_id > 3)
-                                    <div class="card m-1">
+                                    <div class="card m-2 board-card">
                                         <a class=" text-decoration-none text-black"
                                             href="{{ route('tickets.show', $open->id) }}">
-                                            <div class="card-body">
+                                            <div class="card-body shadow">
                                                 <div class="d-flex">
-                                                    <div class="board-pill"
+                                                    <div class="board-pill shadow"
                                                         style="background-color:{{ $open->ticketsImportant->important_bc }};">
                                                         @if ($open->attach_json !== null)
                                                             <i class="bi bi-paperclip fs-4 board-att"
@@ -227,22 +218,19 @@
                                                     {{-- <img src="..." class="card-img-top" alt="..."> --}}
                                                     <h6 class="card-text d-flex"> {{ $open->ticketsUser->name }}</h6>
                                                 </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50">
-                                                    Open: {{ $open->created_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <div class="fs-6 d-flex flex-row text-black-50 ">
-                                                    Edit : {{ $open->updated_at->format('Y-m-d H:i') }}
-                                                </div>
-                                                <h6>{{ $open->title }}</h6>
+                                                <h6 class="mt-3 fw-light">{{ $open->title }}</h6>
                                                 {{-- <p> {!! nl2br(e($open->title)) !!}</p> --}}
                                                 {{-- <h6 class="length_title">{{ $open->title }}</h6> --}}
                                                 {{-- <samp class="card-title">{{ $open->title }}</samp> --}}
                                                 {{-- <p class="card-text">{{ $open->user_id }}</p> --}}
-                                                <p class="length_message">{{ $open->request }}</p>
+                                                <p class="length_message fw-lighter">{{ $open->request }}</p>
                                                 @can('ticket-edit')
                                                     <a class="icon-edit text-decoration-none fs-2 text-black-50 float-end"
                                                         href="{{ route('tickets.edit', $open->id) }}"></a>
                                                 @endcan
+                                                <div class="py-1 fs-6 d-flex flex-row text-black-50 font-monospace">
+                                                    {{ $open->updated_at->format('Y-m-d, H:m') }}
+                                                </div>
                                             </div>
                                         </a>
                                         @can('delete-btn')
